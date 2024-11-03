26c8cb8fca7c1875fd7cb137493c9c47f5b673e5
erosnoxx <garotoslixo@gmail.com>
2024-11-03 04:08:15 -0300

    read

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,6 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,14 +17,13 @@ release = '11/2024'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
+
 extensions = [
-    'myst_parser',  # Para suportar arquivos Markdown
+    'myst_parser',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.githubpages',
 ]
-
-templates_path = ['_templates']
-exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
